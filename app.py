9f3219fd63c6fd218fa5917da9145c77c38b6ff3
p6874435-svg <p6874435@gmail.com>
2026-09-12 01:58:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -360,8 +360,6 @@ footer p { color: #999; font-size: 13px; letter-spacing: 2px; margin-bottom: 20p
         <div style="text-align:center; margin-top:60px" class="reveal">
             <a href="/calculator" class="btn"><span>Рассчитать стоимость →</span></a>
         </div>
-    </div>
-</section>
 
 <section class="contact-section">
     <div class="container">
